0a32661722e9e8ba80935612fd3464a63289b404
Srinath Sadda <Sadda@Outlook.com>
2015-05-06 17:22:45 -0400

    PowerShell RunSpaces - v1.0
    Holds functions to invoke runspaces with multi-threading.

---
+++ PSRunSpaces.psd1
@@ -0,0 +1,96 @@
+﻿#########################################################
+# Module Manifest Name: PSRunSpaces.psd1
+# Module Manifest Description: PowerShell RunSpaces Module
+# Author: Srinath Sadda
+# Version: 1.0
+# Updated: 5/6/2015
+# Copyright © 2015 Srinath Sadda
+#########################################################
+
+@{
+
+# Script module or binary module file associated with this manifest.
+RootModule = '.\PSRunSpaces.psm1'
+
+# Version number of this module.
+ModuleVersion = '1.0.0.0'
+
+# ID used to uniquely identify this module
+GUID = '231b5f47-d05d-4cc8-a2b7-8ce60e4fd398'
+
+# Author of this module
+Author = 'Srinath Sadda'
+
+# Company or vendor of this module
+CompanyName = 'Srinath Sadda'
+
+# Copyright statement for this module
+Copyright = 'Copyright © 2015 Srinath Sadda. All rights reserved'
+
+# Description of the functionality provided by this module
+Description = 'Holds functions to invoke runspaces with multi-threading'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '2.0'
+
+# Name of the Windows PowerShell host required by this module
+# PowerShellHostName = ''
+
+# Minimum version of the Windows PowerShell host required by this module
+PowerShellHostVersion = '1.0'
+
+# Minimum version of Microsoft .NET Framework required by this module
+# DotNetFrameworkVersion = ''
+
+# Minimum version of the common language runtime (CLR) required by this module
+# CLRVersion = ''
+
+# Processor architecture (None, X86, Amd64) required by this module
+# ProcessorArchitecture = ''
+
+# Modules that must be imported into the global environment prior to importing this module
+# RequiredModules = @()
+
+# Assemblies that must be loaded prior to importing this module
+# RequiredAssemblies = @()
+
+# Script files (.ps1) that are run in the caller's environment prior to importing this module.
+# ScriptsToProcess = @()
+
+# Type files (.ps1xml) to be loaded when importing this module
+# TypesToProcess = @()
+
+# Format files (.ps1xml) to be loaded when importing this module
+# FormatsToProcess = @()
+
+# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+NestedModules = @()
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+
+# Variables to export from this module
+VariablesToExport = '*'
+
+# Aliases to export from this module
+AliasesToExport = '*'
+
+# List of all modules packaged with this module
+# ModuleList = @()
+
+# List of all files packaged with this module
+# FileList = @()
+
+# Private data to pass to the module specified in RootModule/ModuleToProcess
+# PrivateData = ''
+
+# HelpInfo URI of this module
+# HelpInfoURI = ''
+
+# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+# DefaultCommandPrefix = ''
+
+}
---
+++ PSRunSpaces.psm1
@@ -0,0 +1,402 @@
+﻿#########################################################
+# Module Name: PSRunSpaces.psm1
+# Module Description: Functions to invoke runspaces with multi-threading
+# Author: Srinath Sadda
+# Version: 1.0
+# Updated: 5/6/2015
+# Copyright © 2015 Srinath Sadda
+#########################################################
+
+# Adds a Microsoft .NET Framework type (a class) to a Windows PowerShell session
+# Initialize a custom powershell class 'AsyncPipeline'
+If (!('AsyncPipeline' -as [Type])) {
+    Add-Type @'
+        public class AsyncPipeline {
+            public System.Management.Automation.PowerShell Pipeline;
+            public System.IAsyncResult AsyncResult;
+        }
+'@
+}
+
+Function Invoke-Async {
+    <#
+        .SYNOPSIS
+            Create a PowerShell pipeline and executes a script block asynchronously.
+        .DESCRIPTION
+            Create a PowerShell pipeline and executes a script block asynchronously.
+        .PARAMETER RunspacePool
+            Specify a pool of one or more runspaces, typically created using 'New-RunspacePool' Cmdlet.
+            A runspace pool is a collection of runspaces upon which PowerShell pipelines can be executed.
+        .PARAMETER ScriptBlock
+            Represents a precompiled block of script text that can be used as a single unit.
+            A script block is an instance of a Microsoft .NET Framework type (System.Management.Automation.ScriptBlock)
+        .PARAMETER Arguments
+            A script block can accept arguments and return values.
+            The 'Arguments' parameter supplies the values of the variables, in the order that they are listed.
+        .EXAMPLE
+            $ScriptBlock = { Param($Computer,$Service) Get-Servie -Name $Service -ComputerName $Computer }
+            Invoke-Async -RunspacePool $(New-RunSpacePool 10) -ScriptBlock $ScriptBlock -Arguments $Computer,$Service
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Specify a pool of one or more runspaces, typically created using 'New-RunspacePool' Cmdlet:")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({
+            If ($_ -is [System.Management.Automation.Runspaces.RunspacePool]) {
+                $True
+            }
+            Else {
+                Throw "OOPS! YOU SPECIFIED AN INCORRECT OBJECT TYPE! THE EXPECTED TYPE IS: [RunspacePool]"
+            }
+        })]
+        [System.Management.Automation.Runspaces.RunspacePool] $RunspacePool,
+        
+        [Parameter(Position=1,Mandatory=$True,HelpMessage="Represents a precompiled block of script text that can be used as a single unit:")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({
+            If ($_ -is [System.Management.Automation.ScriptBlock]) {
+                $True
+            }
+            Else {
+                Throw "OOPS! YOU SPECIFIED AN INCORRECT OBJECT TYPE! THE EXPECTED TYPE IS: [ScriptBlock]"
+            }
+        })]
+        [System.Management.Automation.ScriptBlock] $ScriptBlock,
+        
+        [Parameter(Position=2,Mandatory=$False,HelpMessage="A script block can accept arguments and return values. The 'Arguments' parameter supplies the values of the variables, in the order that they are listed:")]
+        [ValidateNotNullOrEmpty()]
+        [Object[]] $Arguments
+    )
+
+    Try {
+        # Initializes a new instance of the PowerShell class with an empty pipeline.
+        $Pipeline = [System.Management.Automation.PowerShell]::Create()
+        
+        # Sets the runspace pool used by the PowerShell object.
+        # A runspace from this pool is used whenever the PowerShell object pipeline is invoked.
+        $Pipeline.RunspacePool = $RunspacePool
+        
+        # NOTE: Out-Null - Deletes output instead of sending it down the pipeline.
+        # Adds a script to the end of the pipeline of the PowerShell object.
+        $Pipeline.AddScript($ScriptBlock) | Out-Null
+        
+        Foreach($Arg in $Arguments) {
+            # NOTE: Out-Null - Deletes output instead of sending it down the pipeline.
+            # Adds an argument for a positional parameter of a command without specifying the parameter name.
+            $Pipeline.AddArgument($Arg) | Out-Null
+        }
+
+        # Asynchronously runs the commands of the PowerShell object pipeline.
+        $AsyncResult = $Pipeline.BeginInvoke()
+
+        # Creates a AsyncPipeline object.
+        $Status = New-Object AsyncPipeline -ErrorAction Stop -ErrorVariable AsyncPipeline_
+
+        If (!$AsyncPipeline_) {
+            $Status.Pipeline = $Pipeline
+            $Status.AsyncResult = $AsyncResult
+        
+            If ($Status) {
+                # Returns the status of AsyncPipeline
+                Return $Status
+            }
+        }
+    }
+    Catch {
+        # Capture an exception
+        $E = $_.Exception.Message
+        Return $E
+    }
+}
+
+Function New-RunSpacePool {
+    <#
+        .SYNOPSIS
+            Creates a runspace pool.
+        .DESCRIPTION
+            Creates a pool of runspaces that specifies the minimum and maximum number of opened runspaces for the pool.
+        .PARAMETER MaxThreads
+            Defines the maximum number of pipelines that can be concurrently (asynchronously) executed on the pool.
+            The number of available pools determined the maximum number of processes that can be running concurrently.
+        .PARAMETER MTA
+            Create runspaces in a multi-threaded apartment. It is not recommended to use this option unless absolutely necessary.
+        .EXAMPLE
+            Creates a pool of 10 runspaces.
+            $RSPool = New-RunSpacePool -MaxThreads 10
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Specify maximum no. of threads (maximum is 64):")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateRange(1,64)]
+        [Int16] $MaxThreads,
+
+        [Parameter(Position=1,Mandatory=$False,HelpMessage="Specify this switch to create runspaces in a multi-threaded apartment:")]
+        [Switch] $MTA
+    )
+
+    Try {
+        # Creates a pool of runspaces that specifies the minimum and maximum number of opened runspaces for the pool
+        $RSPool = [RunspaceFactory]::CreateRunspacePool(1, $MaxThreads)
+        
+        # NOTE: Runspace.ApartmentState property must be set before the runspace is opened.
+        # Specify the apartment state of the thread used to run commands in the runspace.
+        
+        If ($MTA) {
+            # The thread will create and enter a multi-threaded apartment.
+            $RSPool.ApartmentState = 'MTA'
+        }
+        Else {
+            # The thread will create and enter a single-threaded apartment.
+            $RSPool.ApartmentState = 'STA'
+        }
+
+        # Open runspaces.
+        $RSPool.Open()
+        
+        # Returns a pool of runspaces.
+        Return $RSPool
+    }
+    Catch {
+        # Capture an exception
+        $E = $_.Exception.Message
+        Return $E
+    }
+}
+
+Function Get-AsyncInfo {
+    <#
+        .SYNOPSIS
+            Receives the results of one or more asynchronous pipelines.
+        .DESCRIPTION
+            Receives the results of one or more asynchronous pipelines running in a separate runspaces.
+        .PARAMETER Pipelines
+            An array of AsyncPipleine objects, typically returned by 'Invoke-Async' Cmdlet.
+        .PARAMETER Progress
+            An optional switch to display a progress bar that depicts the status of a running command.
+        .EXAMPLE
+            $ScriptBlock = { Param($Computer,$Service) Get-Servie -Name $Service -ComputerName $Computer }
+            $AsyncPipelines = Invoke-Async -RunspacePool $(New-RunSpacePool 10) -ScriptBlock $ScriptBlock -Arguments $Computer,$Service
+            
+            Get-AsyncInfo -Pipelines $AsyncPipelines -Progress
+        .NOTES
+            Since it is unknown what exists in the results stream of the pipeline, this function will not have a standard return type.
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Specify an array of AsyncPipleine objects, typically returned by Invoke-Async cmdlet:")]
+        [ValidateNotNullOrEmpty()]
+        [AsyncPipeline[]] $Pipelines,
+	    
+        [Parameter(Position=1,Mandatory=$False,HelpMessage="Specify this switch to display a progress bar that depicts the status of a running command:")]
+        [Switch] $Progress
+    )
+
+    # Counter for Write-Progress
+    $I = 1
+
+    ForEach ($Pipeline in $Pipelines) {
+        Try {
+            # NOTE:
+            # Pipeline.EndInvoke - Waits for the pending asynchronous BeginInvoke call to be completed and then returns the results of the call.
+            # AsyncResult - The IAsyncResult interface returned by the BeginInvoke call. This interface represents the status of the call.
+            
+            # Get the results from the IAsyncResult object.
+            $Pipeline.Pipeline.EndInvoke($Pipeline.AsyncResult)
+            
+            # Capture and throw pipeline stream errors.
+            If ($Pipeline.Pipeline.Streams.Error) {
+                Throw $Pipeline.Pipeline.Streams.Error
+            }
+        }
+        Catch {
+            # Capture an exception
+            $E = $_.Exception.Message
+            Return $E
+        }
+        Finally {
+            # Releases all resources used by the PowerShell object.
+            $Pipeline.Pipeline.Dispose()
+
+            If ($Progress) {
+                # Displays a progress bar that depicts the status of a running command.
+                Write-Progress -Activity 'Receiving Results' -PercentComplete $(($I/$Pipelines.Length) * 100) -Status 'Percent Complete'
+            }
+        
+            # Increment counter for Write-Progress
+            $I++
+        }
+    }
+}
+
+Function Get-AsyncStatus {
+    <#
+        .SYNOPSIS
+            Receives the status of one or more asynchronous pipelines.
+        .DESCRIPTION
+            Receives the status of one or more asynchronous pipelines.
+        .PARAMETER Pipelines
+            An array of AsyncPipleine objects, typically returned by 'Invoke-Async' Cmdlet.
+        .EXAMPLE
+            $ScriptBlock = { Param($Computer,$Service) Get-Servie -Name $Service -ComputerName $Computer }
+            $AsyncPipelines = Invoke-Async -RunspacePool $(New-RunSpacePool 10) -ScriptBlock $ScriptBlock -Arguments $Computer,$Service
+
+            $Status = Get-AsyncStatus -Pipelines $AsyncPipelines
+        .EXAMPLE
+            $ScriptBlock = { Param($Computer,$Service) Get-Servie -Name $Service -ComputerName $Computer }
+            
+            $Status = Get-AsyncStatus -Pipelines $(Invoke-Async -RunspacePool $(New-RunSpacePool 10) -ScriptBlock $ScriptBlock -Arguments $Computer,$Service)
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Specify an array of AsyncPipleine objects, typically returned by Invoke-Async Cmdlet:")]
+        [ValidateNotNullOrEmpty()]
+        [AsyncPipeline[]] $Pipelines
+    )
+
+    [Array] $AsyncStatus = @()
+
+    Try {
+        ForEach ($Pipeline in $Pipelines) {
+            [HashTable] $HT = @{
+                'Instance ID' = $Pipeline.Pipeline.Instance_Id
+                'Status' = $Pipeline.Pipeline.InvocationStateInfo.State
+                'Reason' = $Pipeline.Pipeline.InvocationStateInfo.Reason
+                'Completed' = $Pipeline.AsyncResult.IsCompleted
+                'Async State' = $Pipeline.AsyncResult.AsyncState
+                'Error' = $Pipeline.Pipeline.Streams.Error
+            }
+
+            # Create a custom PSObject to hold powershell pipeline info
+            $PipelineInfo = New-Object -TypeName PSObject -Property $HT -ErrorAction Stop -ErrorVariable HT_
+            If (!$HT_) {
+                $AsyncStatus += $PipelineInfo
+            }
+        }
+
+        # Returns the status of one or more asynchronous pipelines.
+        Return $AsyncStatus
+    }
+    Catch {
+        # Capture an exception
+        $E = $_.Exception.Message
+        Return $E
+    }
+}
+
+Function Invoke-PSRunSpaces {
+    <#
+        .SYNOPSIS
+            Executes a set of parameterized script blocks asynchronously using runspaces and returns the resulting data.
+        .DESCRIPTION
+            Encapsulates generic logic for using Powershell background runspaces to execute parameterized script blocks in an efficient multi-threaded fashion.
+        .PARAMETER Servers
+            List of target servers.
+        .PARAMETER ScriptBlock
+            Represents a precompiled block of script text that can be used as a single unit.
+            ScriptBlock should contain one or more parameters.
+            A script block is an instance of a Microsoft .NET Framework type (System.Management.Automation.ScriptBlock)
+        .PARAMETER Arguments
+            A script block can accept arguments and return values.
+            The 'Arguments' parameter supplies the values of the variables, in the order that they are listed.
+        .PARAMETER MaxThreads
+            The maximum number of concurrent threads to use. The default value is equal to no. of specified servers. Maximum is 64.
+        .PARAMETER Progress
+            An optional switch to display a progress bar that depicts the status of a running command.
+        .PARAMETER ServerInstance
+        .EXAMPLE
+            Opens a separate runspace for each server specified in the $Servers variable and executes the ScriptBlock with the specified no. of Arguments.
+            
+            $Servers = @('S1.Contoso.com','S2.Contoso.com')
+            $ScriptBlock = { Param($Computer,$Service) Get-Servie -Name $Service -ComputerName $Computer }
+            
+            Invoke-PSRunSpaces -Servers $Servers -ScriptBlock $ScriptBlock -Arguments $Computer,$Service
+    #>
+
+    [CmdletBinding()]
+    Param (
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Specify a list of servers:")]
+        [ValidateNotNullOrEmpty()]
+        [String[]] $Servers,
+
+        [Parameter(Position=1,Mandatory=$True,HelpMessage="A script block is an instance of a Microsoft .NET Framework type (System.Management.Automation.ScriptBlock):")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({
+            If ($_ -is [System.Management.Automation.ScriptBlock]) {
+                $True
+            }
+            Else {
+                Throw "YOU SPECIFIED A WRONG OBJECT TYPE! THE EXPECTED TYPE IS: [ScriptBlock]"
+            }
+        })]
+        [System.Management.Automation.ScriptBlock] $ScriptBlock,
+
+        [Parameter(Position=2,Mandatory=$False,HelpMessage="A script block can accept arguments and return values:")]
+        [ValidateNotNullOrEmpty()]
+        [Alias("Args")]
+        [Object[]] $Arguments,
+
+        [Parameter(Position=3,Mandatory=$False,HelpMessage="Specify maximum no. of threads (maximum is 64):")]
+        [ValidateNotNullOrEmpty()]
+        [ValidateRange(1,64)]
+        [Int16] $MaxThreads,
+
+        [Parameter(Position=4,Mandatory=$False,HelpMessage="Specify this switch to display a progress bar that depicts the status of a running command:")]
+        [Switch] $Progress,
+
+        [Parameter(Position=5,Mandatory=$False)]
+        [Switch] $ServerInstance
+    )
+
+    Try {
+        # Array variable to store a list of pipelines.
+        $AsyncPipelines = @()
+
+        # Array variable to store result from all runspaces.
+        $Status = @()
+        
+        # Create a pool with specified no. of runspaces.
+        If ($MaxThreads) { $RSPool = New-RunSpacePool -MaxThreads $MaxThreads }
+        
+        # Create a pool with sufficient no. of runspaces (based on no. of servers)
+        Else { $RSPool = New-RunSpacePool -MaxThreads $Servers.Count }
+
+        ForEach ($Server in $Servers) {
+            # Suspends the activity for the specified period of time until a runspace is available.
+            While ($($RSPool.GetAvailableRunspaces()) -le 0) {
+                Start-Sleep -Milliseconds 200
+            }
+            
+            # Create a PowerShell pipeline and executes a script block asynchronously.
+            If (($Arguments) -and ($ServerInstance)) { $AsyncPipelines += Invoke-Async -RunSpacePool $RSPool -ScriptBlock $ScriptBlock -Arguments $Server,$Arguments }
+            ElseIf (($Arguments) -and (!$ServerInstance)) { $AsyncPipelines += Invoke-Async -RunSpacePool $RSPool -ScriptBlock $ScriptBlock -Arguments $Arguments }
+            ElseIf ((!$Arguments) -and ($ServerInstance)) { $AsyncPipelines += Invoke-Async -RunSpacePool $RSPool -ScriptBlock $ScriptBlock -Arguments $Server }
+            Else { $AsyncPipelines += Invoke-Async -RunSpacePool $RSPool -ScriptBlock $ScriptBlock }
+        }
+
+        # Waits for the pending asynchronous BeginInvoke call to be completed in all opened runspaces.
+        While ($(Get-AsyncStatus -Pipelines $AsyncPipelines | Where-Object {$_.Status -eq 'Running'}).Count -gt 0) {
+            Start-Sleep -Milliseconds 200
+        }
+
+        # Get the results of all pipelines running in separate runspaces.
+        If ($Progress) { $Status += Get-AsyncInfo -Pipelines $AsyncPipelines -Progress }
+        Else { $Status += Get-AsyncInfo -Pipelines $AsyncPipelines }
+
+        # Return data.
+        Return $Status
+    }
+    Catch {
+        # Capture an exception
+        $E = $_.Exception.Message
+        Return $E
+    }
+    Finally {
+        # Releases all resources used by the PowerShell object.
+        $RSPool.Dispose()
+    }
+}
